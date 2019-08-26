4198e6f39e891da77f286551f41db0a8197ab013
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-08-27 00:38:41 +0900

    unique.js complete

--- unique/unique.js
+++ unique/unique.js
@@ -1,5 +1,23 @@
+/**pseudo code
+ * 
+ * 요소들을 중복되는 값 없이 하나씩만 추려서 리턴
+ * 
+ * -array 입력
+ * 
+ *  -filter 메소드 활용
+ * 
+ */
+
 function unique(array) {
-  // your code here
+  let uniqueArr = [];
+
+  for(let i = 0; i < array.length; i++) {
+    if(uniqueArr.indexOf(array[i]) === -1) {
+      uniqueArr.push(array[i]);
+    }
+  }
+
+  return uniqueArr;
 }
 
 var output = unique([1,2,2,4,6,6]); // should be [1,2,4,6]
